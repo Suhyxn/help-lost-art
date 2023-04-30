94293a963317568c7bc4eb5356dff1cfb06b563f
Suhyxn <momentomori21489@gmail.com>
2023-04-30 17:37:18 +0900

    conf: Router Settings and Create Page Structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import background from "./background.jpeg";
+import { RecoilRoot } from "recoil";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+body {
+  font-size: 1rem;
+  font-family: 'Noto Sans KR', 'Roboto', sans-serif;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+}
+
+a {
+  color: #000;
+  text-decoration: none;
+}
+
+button {
+  border: none;
+  cursor: pointer;
+  :focus {
+    outline: none;
+  }
+}
+
+input {
+  background-color: transparent;
+  border: none;
+  font-size: 1rem;
+  :focus {
+    outline: none;
+  }
+}
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <GlobalStyle />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
-
-reportWebVitals();
---
+++ src/layout/Home/HomeStyle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeStyle() {
+  return <div>HomeStyle</div>;
+}
+
+export default HomeStyle;
