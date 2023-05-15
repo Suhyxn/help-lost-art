d98e9bd843c73ae6d756e6c17cfaad5859d12844
Suhyxn <momentomori21489@gmail.com>
2023-05-15 23:32:56 +0900

    style: Home.tsx Default layout settings

--- src/style/Home/HomeStyle.tsx
+++ src/style/Home/HomeStyle.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 function HomeStyle() {
-  return <div>HomeStyle</div>;
+  return (
+    <SquareContainer>
+      <SmallSquare>1</SmallSquare>
+      <SmallSquare>2</SmallSquare>
+      <BigSquare>3</BigSquare>
+    </SquareContainer>
+  );
 }
+
+const SquareContainer = styled.div`
+  margin-top: 50px;
+`;
+
+const SmallSquare = styled.div`
+  width: 700px;
+  height: 260px;
+  background-color: #40444f;
+  float: left;
+  margin-left: 90px;
+  margin-bottom: 50px;
+`;
+
+const BigSquare = styled.div`
+  background-color: #40444f;
+  width: 1490px;
+  height: 260px;
+  clear: left;
+  margin: 0 auto;
+`;
 
 export default HomeStyle;
