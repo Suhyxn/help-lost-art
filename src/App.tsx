38b768722bf6a8e2fa7bce49c479bc9e8a843a7d
Suhyxn <momentomori21489@gmail.com>
2023-07-18 12:57:04 +0900

    feat: Update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
+import AbyssDungeons from "components/GameContents/AbyssDungeons";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/challenge-abyss" element={<AbyssDungeons />} />
         </Routes>
       </BrowserRouter>
     </div>
